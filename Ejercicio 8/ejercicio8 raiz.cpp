001ad5e7b1f3011063a556108360ece55c3b923a
Djorkaeffhndz <106544793+Djorkaeffhndz@users.noreply.github.com>
2022-07-18 22:18:32 -0600

    Add files via upload

---
+++ Ejercicio 8/ejercicio8 raiz.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double numeroRaiz(double n1)// funcion 
+{
+	return sqrt(n1);
+}
+int main()
+{
+	
+	double numero = 0, raiz = 0;
+	cout<<"Ingrese un numero para poder saber su raiz: ";//pedimos al usuario un numero
+	cin>>numero;
+	
+	if(numero < 0)
+	{
+		cout<<"Esta raiz no existe";
+	}
+	else
+	{
+		cout<<"La raiz de "<<numero<<" es: "<<numeroRaiz(numero);
+	}
+	return 0;
+}
+
